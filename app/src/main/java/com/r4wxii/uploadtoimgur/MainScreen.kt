44e18c5eaf317265ae7073326110805446efab75
r4wxii <yn041010@gmail.com>
2022-08-06 22:45:42 +0900

    ギャラリーから画像を選択して表示

--- app/src/main/java/com/r4wxii/uploadtoimgur/MainScreen.kt
+++ app/src/main/java/com/r4wxii/uploadtoimgur/MainScreen.kt
@@ -1,10 +1,42 @@
 package com.r4wxii.uploadtoimgur
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import coil.compose.AsyncImage
 
 @Composable
 fun MainScreen() {
-    Box(Modifier.fillMaxSize())
+    val (imageUri, setImageUri) = remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val launcher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.GetContent(),
+            onResult = setImageUri,
+        )
+
+    Box(Modifier.fillMaxSize()) {
+        val mod = Modifier
+            .fillMaxSize(.6f)
+            .align(Alignment.Center)
+
+        Box(
+            mod
+                .background(Color.Gray)
+                .clickable {
+                    launcher.launch("image/*")
+                },
+        )
+        imageUri?.let {
+            AsyncImage(model = imageUri, contentDescription = "", modifier = mod)
+        }
+    }
 }
