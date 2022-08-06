f79590859ad7c7fe68ab65de6514ec958bf7ee66
r4wxii <yn041010@gmail.com>
2022-08-06 22:13:32 +0900

    init

---
+++ app/src/main/java/com/r4wxii/uploadtoimgur/MainScreen.kt
@@ -0,0 +1,10 @@
+package com.r4wxii.uploadtoimgur
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen() {
+    Box(Modifier.fillMaxSize())
+}
