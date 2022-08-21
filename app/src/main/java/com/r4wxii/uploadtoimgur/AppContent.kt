fa47abf791439dd944882673221db1dfc3f15c85
r4wxii <yn041010@gmail.com>
2022-08-21 18:46:31 +0900

    OAuth認証のコールバックを受け取るdeeplinkを追加

--- app/src/main/java/com/r4wxii/uploadtoimgur/AppContent.kt
+++ app/src/main/java/com/r4wxii/uploadtoimgur/AppContent.kt
@@ -1,14 +1,29 @@
 package com.r4wxii.uploadtoimgur
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 
 @Composable
 fun AppContent() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "main") {
         composable("main") { MainScreen() }
+        composable(
+            "authCallback?query={query}",
+            deepLinks = listOf(navDeepLink {
+                uriPattern =
+                    "app://com.r4wxii.uploadtoimgur/authCallback#{query}"
+            }),
+            arguments = listOf(
+                navArgument("query") { nullable = true },
+            ),
+        ) { backStackEntry ->
+            Text(backStackEntry.arguments?.getString("query") ?: "")
+        }
     }
 }
