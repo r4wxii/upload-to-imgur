c429020d563e938ed17242259766d4608c1750b8
r4wxii <yn041010@gmail.com>
2022-08-27 23:56:17 +0900

    Imgurの認証ページを表示

--- app/src/main/java/com/r4wxii/uploadtoimgur/MainScreen.kt
+++ app/src/main/java/com/r4wxii/uploadtoimgur/MainScreen.kt
@@ -1,15 +1,20 @@
 package com.r4wxii.uploadtoimgur
 
+import android.content.Intent
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 
 @Composable
@@ -23,10 +28,15 @@ fun MainScreen() {
             onResult = setImageUri,
         )
 
-    Box(Modifier.fillMaxSize()) {
+    val context = LocalContext.current
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+    ) {
         val mod = Modifier
             .fillMaxSize(.6f)
-            .align(Alignment.Center)
 
         Box(
             mod
@@ -37,6 +47,16 @@ fun MainScreen() {
         )
         imageUri?.let {
             AsyncImage(model = imageUri, contentDescription = "", modifier = mod)
+        }
+        Spacer(Modifier.height(height = 16.dp))
+        Button(onClick = {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://api.imgur.com/oauth2/authorize?client_id=${BuildConfig.imgurClientId}&response_type=token")
+            )
+            context.startActivity(intent)
+        }) {
+            Text("認証")
         }
     }
 }
